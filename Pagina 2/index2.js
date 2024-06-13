a1e201c0472b861b1a0f22e0f6c201b9f52cf965
Altieri Palorca <altierirpalorca@gmail.com>
2024-06-13 19:36:47 -0300

    1.0 - commit inicial

---
+++ Pagina 2/index2.js
@@ -0,0 +1,11 @@
+let nomeDesp = document.querySelector('#nomeDesp').addEventListener('input', function(event){
+
+})
+
+let valorDesp = document.querySelector('#valorDesp').addEventListener('input', function(event){
+
+})
+
+let buttonEnviar = document.querySelector('#enviarDesp').addEventListener('click', function(event){
+    window.location.href = 'http://www.youtube.com';
+})
---
+++ Página 1/index.js
@@ -0,0 +1,11 @@
+let usuario = document.querySelector('#usuario').addEventListener('input', function(event){
+
+})
+
+let senha = document.querySelector('#senha').addEventListener('input', function(event){
+
+})
+
+let buttonEnviar = document.querySelector('#buttonEnviar').addEventListener('click', function(event){
+    window.location.href = 'http://www.uol.com.br';
+})
